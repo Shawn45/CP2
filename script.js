7fc68b5b402aad32614c543166d261fbcb8bc617
Shawn45 <byun2@byu.edu>
2020-02-22 16:07:08 -0700

    First Commit

--- script.js
+++ script.js
@@ -5,57 +5,56 @@ document.getElementById("weatherSubmit").addEventListener("click", function(even
     return;
   //console.log(value);
   //fetch the weather
-  const url = "http://api.openweathermap.org/data/2.5/weather?q=" + value + ",US&units=imperial" + "&APPID=504a9f57bba961aa706a47683f8a813f";
-  fetch(url)
-    .then(function(response) {
-      return response.json();
-    }).then(function(json) {
-      //console.log(json);
-      let results = "";
-      results += '<h2>Today ' + moment(1000*json.dt).format('MMMM Do YYYY') + "</h2>"; 
-      results += '<h2>Weather in ' + json.name + "</h2>";
 
-      results += '<h3>Temperature: ' + json.main.temp + " &deg;F</h3>"
-      results += '<h3>Feels like:    ' + json.main.feels_like + " &deg;F</h3>"
-      for (let i=0; i < json.weather.length; i++) {
-	results += '<img src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
-      }
-      results += "<p>"
-      for (let i=0; i < json.weather.length; i++) {
-	results += json.weather[i].description
-	if (i !== json.weather.length - 1)
-	  results += ", "
-      }
-      results += "</p>";
-      results += '<h3>Clouds: ' + json.clouds.all + " %</h3>";
-      results += '<h3>Humidity: ' + json.main.humidity + " %</h3>";
-      results += '<h3>Wind speed: ' + json.wind.speed + " m/h</h3>";
-      results += '<h3>Sunrise ' + moment(1000*json.sys.sunrise).format('h:mm:ss a') + "</h3>";
-      results += '<h3>Sunset ' + moment(1000*json.sys.sunset).format('h:mm:ss a') + "</h3>";
+    let today = new Date();
+    let year = today.getFullYear()
+    let month = today.getMonth()+1
+    if(month < 10) {
+      month = "0" + month
+    }
 
-      document.getElementById("weatherResults").innerHTML = results;
-    });
+    let days = today.getDate();
+    let todayDate = year + month + days;
+
+    const url3 = "https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=" + value + "&facet_field=day_of_week&facet=true&begin_date=" + todayDate + "&end_date=" + todayDate +"&api-key=A9GvUQ0sCKiROdI0LvnAvLPfwVVQAPEZ";
+    fetch(url3)
+      .then(function(response) {
+        return response.json();
+      }).then(function(json) {
+       // console.log(json);
+       let results = "";
+       if(json.response.docs.length !== 0) {
+         for (let i=0; i < json.response.docs.length; i++) {
+           results += "<div id=\"aticle\">";
+
+           results += "<a href=\"" + json.response.docs[i].web_url +"\">";
+           results += "<h2>" + json.response.docs[i].headline.main + "</h2>";
+           results += "</a>";
 
 
-  const url2 = "http://api.openweathermap.org/data/2.5/forecast?q=" + value + ", US&units=imperial" + "&APPID=504a9f57bba961aa706a47683f8a813f";
-  fetch(url2)
-    .then(function(response) {
-      return response.json();
-    }).then(function(json) {
-     // console.log(json);
-     let forecast = "";
-	forecast += "<h2>5-Day Forecast:</h2>"
-      for (let i=0; i < json.list.length; i++) {
-	forecast += "<h2>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY, h a') + "</h2>";
-	forecast += "<p>Temperature: " + json.list[i].main.temp + " &deg;F</p>";
-	forecast += "<p>Feels like:  " + json.list[i].main.feels_like + " &deg;F</p>";
-	forecast += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/>';
-	forecast += '<p>Clouds: ' + json.list[i].clouds.all + " %</p>";
-	forecast += '<p>Humidity: ' + json.list[i].main.humidity + " %</p>";
-	forecast += '<p>Wind speed: ' + json.list[i].wind.speed + " m/h</p>";
+           results += "<p>" + json.response.docs[i].lead_paragraph + "</p>";
+           results += "<p>catagory: " + json.response.docs[i].section_name;
+           if(json.response.docs[i].subsection_name !== undefined) {
+             results += " " + json.response.docs[i].subsection_name+"</p>";
+           }
+           results += "<a href=\"" + json.response.docs[i].web_url +"\">";
+           results += "<h5>" + json.response.docs[i].web_url + "</h5>";
+           results += "</a>";
 
-      }
-      document.getElementById("forecastResults").innerHTML = forecast;
-    });
+
+           results += "</div>";
+         }
+         results += "<div id=\"resultsOfSearch\">";
+         results += "<p>" + json.response.docs.length + " articles found</p>";
+         results += "</div>";
+       }
+       else {
+         results += "<div id=\"resultsOfSearch\">";
+         results += "<p>No articles found with \'" + value + "\'</p>";
+         results += "<p>Please search again!</p>";
+         results += "</div>";
+       }
+
+        document.getElementById("allArticles").innerHTML = results;
+      });
 });
-
